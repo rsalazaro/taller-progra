9c4331da1212367d564ce26eb987d40b0bd17fa0
rsalazaro <r.alejandro.salazaro@gmail.com>
2020-05-05 21:33:58 -0400

    Modificación ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,16 @@
-cm = float(input('Ingrese la cantidad de centímetros: '))
-print(cm, 'centímetros son', cm/2.54,'pulgadas')
+#Ejercicio 1: Escriba un algoritmo que solicite cuatro números y calcule la media aritmética (promedio) entre ellos.
+
+# Entradas
+
+numero1 = float(input('Ingrese número 1: '))
+numero2 = float(input('Ingrese número 2: '))
+numero3 = float(input('Ingrese número 3: '))
+numero4 = float(input('Ingrese número 4: '))
+
+# Proceso
+
+promedio = (numero1 + numero2 + numero3 + numero4)/4
+
+# Salidas
+
+print('El promedio de los 4 números es', promedio)
